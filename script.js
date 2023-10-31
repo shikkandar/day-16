1cfdfc114cfec44ab2b4f4b76128fdd76f98880d
shikkandar <mohammedshikkandar1@gmail.com>
2023-10-31 18:12:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const countdownElement = document.getElementById("countdown");
+
+    function countdown(num, callback) {
+        if (num > 0) {
+            countdownElement.innerText = num;
+            setTimeout(function() {
+                countdown(num - 1, callback);
+            }, 1000); // Wait for 1 second
+        } else {
+            callback();
+        }
+    }
+
+    countdown(10, function() {
+        countdownElement.innerText = "Happy Independence Day";
+    });
+});
